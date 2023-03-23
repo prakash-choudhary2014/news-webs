4dde4b68edcaa5249a44e7c604eb77bc0363cc1b
Prakash choudhary <85573801+prakash-choudhary2014@users.noreply.github.com>
2023-03-23 22:28:48 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import NewsPage from '@/components/NewsPage'
 export default function Home() {
   return (
     <>
-    <h1>Welcome to News mania</h1>
+    <h1 style={{fontSize:50, fontFamily:"unset", fontStyle:"italic"}}> News-Mania</h1>
     <NewsPage/>
     </>
   )
